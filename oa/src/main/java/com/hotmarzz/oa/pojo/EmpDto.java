bcf01d2663389dd9e38baeb0028917f1c83011db
jinmindatc <jinmindatc@163.com>
2017-07-03 16:27:38 +0800

    员工修改密码完毕，新加一个EmpDto用来接收新老密码

---
+++ oa/src/main/java/com/hotmarzz/oa/pojo/EmpDto.java
@@ -0,0 +1,46 @@
+package com.hotmarzz.oa.pojo;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class EmpDto {
+
+	private Long empId;
+	@NotEmpty(message="旧密码不能为空")
+	private String oldPwd;
+	@NotEmpty(message="新密码不能为空")
+	private String newPwd;
+	@NotEmpty(message="检验新密码不能为空")
+	private String ckNewPwd;
+	
+	
+	@Override
+	public String toString() {
+		return "EmpDto [empId=" + empId + ", oldPwd=" + oldPwd + ", newPwd="
+				+ newPwd + ", ckNewPwd=" + ckNewPwd + "]";
+	}
+	public Long getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+	public String getOldPwd() {
+		return oldPwd;
+	}
+	public void setOldPwd(String oldPwd) {
+		this.oldPwd = oldPwd;
+	}
+	public String getNewPwd() {
+		return newPwd;
+	}
+	public void setNewPwd(String newPwd) {
+		this.newPwd = newPwd;
+	}
+	public String getCkNewPwd() {
+		return ckNewPwd;
+	}
+	public void setCkNewPwd(String ckNewPwd) {
+		this.ckNewPwd = ckNewPwd;
+	}
+	
+}
